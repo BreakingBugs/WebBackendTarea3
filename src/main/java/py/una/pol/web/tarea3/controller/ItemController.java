3880c086d35bddbb7e6281ac54ee0c88360ff772
Jordan <jayala1993@outlook.com>
2016-03-12 14:58:34 -0300

    Insertar duplicados

--- src/main/java/py/una/pol/web/tarea3/controller/ItemController.java
+++ src/main/java/py/una/pol/web/tarea3/controller/ItemController.java
@@ -1,12 +1,16 @@
 package py.una.pol.web.tarea3.controller;
 
+import py.una.pol.web.tarea3.exceptions.DuplicateException;
 import py.una.pol.web.tarea3.model.Item;
 import py.una.pol.web.tarea3.model.Provider;
 
 import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -22,6 +26,12 @@ public class ItemController {
     @Inject
     ProviderController providerController;
 
+    @Inject
+    DuplicateItemController duplicateItemController;
+
+    @Inject
+    private ItemController self;
+
     public List<Item> getItems() {
         CriteriaBuilder cb = em.getCriteriaBuilder();
         CriteriaQuery<Item> cq = cb.createQuery(Item.class);
@@ -32,12 +42,38 @@ public class ItemController {
         return query.getResultList();
     }
 
+    public Item getItemByName(String name) {
+        CriteriaBuilder cb = em.getCriteriaBuilder();
+        CriteriaQuery<Item> cq = cb.createQuery(Item.class);
+        Root<Item> root = cq.from(Item.class);
+        cq.select(root);
+        cq.where(cb.equal(root.get("name"), name));
+        TypedQuery<Item> query = em.createQuery(cq);
+        Item i = query.getSingleResult();
+
+        return i;
+    }
+
     public void addItem(Item p) {
         if (p.getProvider() != null) {
             Provider provider = providerController.getProvider(p.getProvider().getId());
             p.setProvider(provider);
         }
-        em.persist(p);
+        try {
+            self.tryAddItem(p);
+        } catch (DuplicateException e) {
+            duplicateItemController.addDuplicate(p);
+        }
+    }
+
+
+    @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW) //BMT en una trx
+    public void tryAddItem(Item p) throws DuplicateException {
+        try {
+            em.persist(p);
+        } catch (PersistenceException e) {
+            throw new DuplicateException();
+        }
     }
 
     public Item getItem(Integer id) {
