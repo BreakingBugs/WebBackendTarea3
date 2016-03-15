5e75d13578da23061eeba912aaa3e84056438ec5
Guillermo Peralta <gperaltascura@gmail.com>
2016-03-15 16:59:12 -0300

    Single path for adding items

--- src/main/java/py/una/pol/web/tarea3/rest/ItemService.java
+++ src/main/java/py/una/pol/web/tarea3/rest/ItemService.java
@@ -1,11 +1,20 @@
 package py.una.pol.web.tarea3.rest;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import py.una.pol.web.tarea3.controller.ItemController;
 import py.una.pol.web.tarea3.model.Item;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 @Path("/productos")
@@ -22,17 +31,44 @@ public class ItemService {
     @POST
     @Consumes("application/json")
     @Produces("application/json")
-    public Item addItem(Item newItem) {
-        itemController.addItem(newItem);
-        return newItem;
+    public Response addItems(String jsonString) {
+        ObjectMapper om = new ObjectMapper();
+
+        try {
+            JsonNode node = om.readValue(jsonString, JsonNode.class);
+            if(node.isArray()) {
+                List<Item> items = Arrays.asList(om.readValue(jsonString, Item[].class));
+                addBatch(items);
+                return Response.ok(items).build();
+            } else {
+                Item item = om.readValue(jsonString, Item.class);
+                addSingle(item);
+                return Response.ok(item).build();
+            }
+        } catch (JsonMappingException e) {
+            Logger logger = LoggerFactory.getLogger(ItemService.class);
+            logger.error(e.getMessage());
+            e.printStackTrace();
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        } catch (JsonParseException e) {
+            Logger logger = LoggerFactory.getLogger(ItemService.class);
+            logger.error(e.getMessage());
+            e.printStackTrace();
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        } catch (IOException e) {
+            Logger logger = LoggerFactory.getLogger(ItemService.class);
+            logger.error(e.getMessage());
+            e.printStackTrace();
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
     }
 
-    @POST
-    @Consumes("application/json")
-    @Produces("application/json")
-    public Response addItem(List<Item> items) {
-        int duplicates = itemController.batchAddItem(items);
-        return Response.ok(items).build();
+    private void addBatch(List<Item> items) {
+        itemController.batchAddItem(items);
+    }
+
+    private void addSingle(Item item) {
+        itemController.addItem(item);
     }
 
     @GET
